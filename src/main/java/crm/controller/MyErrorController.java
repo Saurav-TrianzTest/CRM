cb6e9c73297c2dfc87948b134be26f31404ae7b6
Modernize Studio <studio@modernize.com>
2026-02-04 18:18:09 +0000

    Commit for planId: 405

--- src/main/java/crm/controller/MyErrorController.java
+++ src/main/java/crm/controller/MyErrorController.java
@@ -1,22 +1,31 @@
 package crm.controller;
 
-import org.springframework.boot.autoconfigure.web.ErrorController;
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.http.HttpServletRequest;
 
 @RestController
 public class MyErrorController implements ErrorController {
 
-    private static final String PATH = "/error";
+    @RequestMapping(value = "/error")
+    public ResponseEntity<String> handleError(HttpServletRequest request) {
+        Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
 
-    @RequestMapping(value = PATH)
-    public String error() {
-        return "Error handling";
-    }
+        if (status != null) {
+            int statusCode = Integer.parseInt(status.toString());
 
-    @Override
-    public String getErrorPath() {
-        return PATH;
+            if(statusCode == HttpStatus.NOT_FOUND.value()) {
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Error 404 - Not Found");
+            } else if(statusCode == HttpStatus.INTERNAL_SERVER_ERROR.value()) {
+                return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error 500 - Internal Server Error");
+            }
+        }
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error handling");
     }
 
 }
